1bc4e6ec057ffcee7b70254a1308399f091f5cde
Anthony Cox <anthony@tlcsoftware.net>
2021-05-05 16:22:23 -0500

    added documentation to the tests

--- WeatherTest/UnitTest1.cs
+++ WeatherTest/UnitTest1.cs
@@ -1,12 +1,31 @@
+// ***********************************************************************
+// Assembly         : WeatherTest
+// Author           : Anthony Cox
+// Created          : 05-05-2021
+//
+// Last Modified By : Anthony Cox
+// Last Modified On : 05-05-2021
+// ***********************************************************************
+// <copyright file="UnitTest1.cs" company="TLC Software.net, LLC">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Text;
 
 namespace WeatherTest
 {
+    /// <summary>
+    /// Defines test class UnitTest1.
+    /// </summary>
     [TestClass]
     public class UnitTest1
     {
+        /// <summary>
+        /// Defines the test method testWeatherOutput.
+        /// </summary>
         [TestMethod]
         public void testWeatherOutput()
         {
